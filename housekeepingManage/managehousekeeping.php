eeffdb076554665dc0097487d3697d69eb14e0cf
Daxon-us <89588551+Daxon-us@users.noreply.github.com>
2022-05-08 22:39:03 +0530

    Add files via upload

---
+++ housekeepingManage/managehousekeeping.php
@@ -0,0 +1,60 @@
+<?php 
+include "config.php";
+
+    //get data from users table
+    $sql = "SELECT * FROM item";
+    //execute the query
+    $result = $conn->query($sql);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>View Page</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+    <link rel="stylesheet" type="text/css"  href="css/read.css">
+</head>
+
+<body>
+
+<?php include 'headerBar.php';?>
+
+        <div class="container">
+            <h1>Manage Housekeeping</h1>
+            <button class="btn btn-primary my-5"><a href="itemAdd.php">Add Room</a></button>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Room Id</th>
+                        <th>Room Status</th>
+                        <th>Room Type</th>
+                        <th>Price(Rs.)</th>
+                        <th>Option</th>
+                    </tr>
+                </thead>
+                <tbody Id="output">
+                    <?php
+                        if ($result->num_rows > 0) {
+                            while ($row = $result->fetch_assoc()) {
+                    ?>
+                            <tr>
+                                <td><?php echo $row['Id']; ?></td>
+                                <td><?php echo $row['Status']; ?></td>
+                                <td><?php echo $row['Type']; ?></td>
+                                <td><?php echo $row['Remarks']; ?></td>
+                                <td><a class="btn btn-danger" href="itemDelete.php?Id=<?php echo $row['Id']; ?>">Delete</a><td>
+                            </tr>
+                    <?php     
+                            }   
+                        }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </body>
+</html>
---
+++ housekeepingManage/roomUpdate.php
@@ -0,0 +1,83 @@
+<?php
+include "config.php";
+
+    	if (isset($_POST['update'])) {
+			$type = $_POST['type'];
+			$user_id = $_POST['id'];
+			$status = $_POST['status'];
+			$availability = $_POST['availability'];
+			$reservation = $_POST['reservation'];
+			$remarks = $_POST['remarks'];
+
+			// update
+			$sql = "UPDATE `supplier` SET `type`='$type',`status`='$status',`availability`='$availability',`reservation`='$reservation',`remarks`='$remarks' WHERE `id`='$user_id'";
+			// execute the query
+			$result = $conn->query($sql);
+
+			if ($result == TRUE) {
+				header('Location: supRead.php');
+			}else{
+				echo "Error:" . $sql . "<br>" . $conn->error;
+			}
+		}
+		
+		if (isset($_GET['id'])) {
+			$user_id = $_GET['id'];
+			// get user data
+			$sql = "SELECT * FROM `supplier` WHERE `id`='$user_id'";
+			// execute the sql
+			$result = $conn->query($sql);
+
+			if ($result->num_rows > 0) {
+				while ($row = $result->fetch_assoc()) {
+					$type = $row['name'];
+					$statuss = $row['status'];
+					$availability = $row['availability'];
+					$reservation  = $row['reservation'];
+					$remarks = $row['remarks'];
+					$id = $row['id'];
+				}
+	?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+    	<link rel="stylesheet" type="text/css"  href="css/add.css">
+    	<title>Update Room Details</title>
+	</head>
+	<body>
+	<?php include 'headerBar.php';?>
+		<h1>Update Room Details</h1>
+			<form action="" method="post">
+			<div class="container">
+			<label for="name"> <h4>Enter Room Type: </h4></label><br>
+				<div class="w-50">
+					<input type="text" class="form-control" placeholder="Enter type" name="type" value="<?php echo $type; ?>" required></div>
+					<input type="hidden" name="id" value="<?php echo $user_id; ?>">
+					<label for="name"> <h4>Enter room Status: </h4></label><br>
+				<div class="w-50">
+					<input type="text" class="form-control" placeholder="Enter Status" name="status" value="<?php echo $status; ?>" required></div>
+					<label for="name"> <h4>Enter Availability: </h4> </label><br>
+				<div class="w-50">
+					<input type="mobile" maxlength="10" minlength="10" class="form-control" placeholder="Enter details" name="availability" value="<?php echo $availability; ?>" required></div>
+					<label for="name"> <h4>Enter Reservation Status: </h4></label><br>
+				<div class="w-50">
+					<input type="email" class="form-control" placeholder="Pending/Confirmed" name="reservation" value="<?php echo $reservation; ?>" required></div>
+					<label for="name"> <h4>Additional Remarks: </h4></label><br>  
+				<div class="w-50">
+					<input type="item" class="form-control" placeholder="Enter details" name="remarks" value="<?php echo $remarks; ?>" required></div>
+					<div class="btnset">
+					<input type="submit" class="btn btn-primary" value="Update" name="update" >
+			</div>
+			</div>
+			</form>
+	</body>
+</html>
+
+<?php
+	} 
+	else{
+		// If the 'id' value is not valid, redirect the user back to read.php page
+		header('Location: supRead.php');
+	}
+}
+?>
