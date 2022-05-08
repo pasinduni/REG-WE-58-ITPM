afa588cfc0c4d3ca63cec07e84b1708795c4db2f
dulanllk <77153337+dulanllk@users.noreply.github.com>
2022-05-08 17:17:58 +0530

    Create database.php

---
+++ User Management/config/database.php
@@ -0,0 +1,19 @@
+<?php
+
+const DB_HOST = 'localhost';
+const DB_NAME = 'itpm';
+const DB_USER = 'root';
+const DB_PASSWORD = '';
+
+
+$databaseHost = 'localhost';
+$databaseName = 'itpm';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+
+
+
+
+
